5b51ced83d08741608d3d4ca8565f7b830323adb
scardonau <40206329+scardonau@users.noreply.github.com>
2018-06-18 11:07:02 -0500

    Put and Patch methods tested against github api (#10)
    * reusing put and get requests on GithubApi.put
    
    * put and patch methods tested
    
    * fixed auth for travis
    
    * code refactored after review

---
+++ test/GithubApi.Issue.test.js
@@ -0,0 +1,70 @@
+const agent = require('superagent-promise')(require('superagent'), Promise);
+const chai = require('chai');
+
+const { expect } = chai;
+
+describe('Given a user logged in Github', () => {
+  const baseUrl = 'https://api.github.com';
+  let user;
+  let reposUrl;
+  let firstRepository;
+
+  describe('When we get the logged in user', () => {
+    before(() => agent.get(`${baseUrl}/user`)
+      .auth('token', process.env.ACCESS_TOKEN)
+      .then((response) => {
+        user = response.body;
+        reposUrl = user.repos_url;
+      }));
+
+    it('Should have at least one public repo', () => {
+      expect(user.public_repos).to.be.above(0);
+    });
+  });
+
+  describe('When we get his repos list', () => {
+    before(() => agent.get(reposUrl)
+      .auth('token', process.env.ACCESS_TOKEN)
+      .then((response) => {
+        [firstRepository] = response.body;
+      }));
+
+    it('At least one repo must exist', () => {
+      expect(firstRepository).to.have.a.property('name');
+    });
+  });
+
+  describe('When we try to create an issue on the repository', () => {
+    const expectedTitle = 'test issue';
+    let createdIssue;
+
+    before(() => agent.post(`${baseUrl}/repos/${user.login}/${firstRepository.name}/issues`)
+      .auth('token', process.env.ACCESS_TOKEN)
+      .send({ title: expectedTitle })
+      .then((response) => {
+        createdIssue = response.body;
+      }));
+
+    it(`Should create an issue with the title ${expectedTitle} and empty body`, () => {
+      expect(createdIssue.body).to.equal(null);
+      expect(createdIssue.title).to.equal(expectedTitle);
+    });
+
+    describe('And we try to modify the issue body', () => {
+      const expectedBody = 'Now I have a body';
+      let modifiedIssue;
+
+      before(() => agent.patch(`${baseUrl}/repos/${user.login}/${firstRepository.name}/issues/${createdIssue.number}`)
+        .auth('token', process.env.ACCESS_TOKEN)
+        .send({ body: expectedBody })
+        .then((response) => {
+          modifiedIssue = response.body;
+        }));
+
+      it('The issue body must be correctly modified', () => {
+        expect(modifiedIssue.body).to.equal(expectedBody);
+        expect(modifiedIssue.title).to.equal(expectedTitle);
+      });
+    });
+  });
+});
--- test/GithubApi.Put.test.js
+++ test/GithubApi.Put.test.js
@@ -8,60 +8,64 @@ describe('Given a user logged in github', () => {
   const userName = 'aperdomob';
   const baseUrl = 'https://api.github.com';
 
+  let queryResponse;
+
+  function putQuery() {
+    return agent.put(`${baseUrl}/user/following/${userName}`)
+      .auth('token', process.env.ACCESS_TOKEN);
+  }
+
+  function getQuery() {
+    return agent.get(`${baseUrl}/user/following`)
+      .auth('token', process.env.ACCESS_TOKEN)
+      .then(response => response.body);
+  }
 
   describe(`When we try to follow ${userName}`, () => {
-    let queryResponse;
-
-    before(() => agent.put(`${baseUrl}/user/following/${userName}`)
-      .auth('token', process.env.ACCESS_TOKEN)
-      .then((response) => {
-        queryResponse = response;
-      }));
+    before(async () => {
+      queryResponse = await putQuery();
+    });
 
     it('Then we should get a valid response', () => {
       expect(queryResponse.statusCode).to.equal(statusCode.NO_CONTENT);
       expect(queryResponse.body).to.eql({});
     });
 
-    let foundUser;
+    describe('And if we check the followed ussers', () => {
+      let foundUser;
 
-    before(() => agent.get(`${baseUrl}/user/following`)
-      .auth('token', process.env.ACCESS_TOKEN)
-      .then((response) => {
-        const followedUsers = response.body;
+      before(async () => {
+        const followedUsers = await getQuery();
         foundUser = followedUsers.find(user => user.login === userName);
-      }));
+      });
 
-    it(`And should be folowing ${userName}`, () => {
-      expect(foundUser.login).to.equal(userName);
+      it(`We should be following ${userName}`, () => {
+        expect(foundUser.login).to.equal(userName);
+      });
     });
   });
 
   describe('If we try to follow him again', () => {
-    let queryResponse;
-
-    before(() => agent.put(`${baseUrl}/user/following/${userName}`)
-      .auth('token', process.env.ACCESS_TOKEN)
-      .then((response) => {
-        queryResponse = response;
-      }));
+    before(async () => {
+      queryResponse = await putQuery();
+    });
 
     it('We can verify if put method is idempotent', () => {
       expect(queryResponse.statusCode).to.equal(statusCode.NO_CONTENT);
       expect(queryResponse.body).to.eql({});
     });
 
-    let foundUser;
+    describe('And when we check the followed users', () => {
+      let foundUser;
 
-    before(() => agent.get(`${baseUrl}/user/following`)
-      .auth('token', process.env.ACCESS_TOKEN)
-      .then((response) => {
-        const followedUsers = response.body;
+      before(async () => {
+        const followedUsers = await getQuery();
         foundUser = followedUsers.find(user => user.login === userName);
-      }));
+      });
 
-    it(`And should still be folowing ${userName}`, () => {
-      expect(foundUser.login).to.equal(userName);
+      it(`It should still be following ${userName}`, () => {
+        expect(foundUser.login).to.equal(userName);
+      });
     });
   });
 });
