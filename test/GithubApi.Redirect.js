cdc17bb69ce6b772396504b6f302f21f435b4278
Santiago Cardona Urrea <scardonau@pslcol.com.co>
2018-06-19 17:50:56 -0500

    head request and redirecting

---
+++ test/GithubApi.Redirect.js
@@ -0,0 +1,35 @@
+const agent = require('superagent-promise')(require('superagent'), Promise);
+const statusCode = require('http-status-codes');
+const { expect } = require('chai');
+
+describe('Given an old repo that changed name', () => {
+  const oldUrl = 'https://github.com/aperdomob/redirect-test';
+  const newUrl = 'https://github.com/aperdomob/new-redirect-test';
+
+  describe('When we consult the repo via HEAD services', () => {
+    let headQueryResponse;
+    before(() => agent.head(oldUrl)
+      .catch((err) => {
+        headQueryResponse = err;
+      }));
+
+    it('We verify that the repo did change, and get the new url', () => {
+      expect(headQueryResponse.status).to.equal(statusCode.MOVED_PERMANENTLY);
+      expect(headQueryResponse.response.headers.location).to.equal(newUrl);
+    });
+  });
+
+  describe('And if we visit the old repo via GET service', () => {
+    let getQueryResponse;
+
+    before(() => agent
+      .get(oldUrl)
+      .then((response) => {
+        getQueryResponse = response;
+      }));
+
+    it('The repo must redirect correctly to the new url', () => {
+      expect(getQueryResponse.status).to.equal(statusCode.OK);
+    });
+  });
+});
