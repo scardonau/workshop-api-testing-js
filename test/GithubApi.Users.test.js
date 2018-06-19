8dc926ad0b64843e68abfc94a739da1434122da1
Santiago Cardona Urrea <scardonau@pslcol.com.co>
2018-06-19 18:17:37 -0500

    response time implemented

---
+++ test/GithubApi.Users.test.js
@@ -0,0 +1,20 @@
+const agent = require('superagent-promise')(require('superagent'), Promise);
+const chai = require('chai');
+const responseTime = require('superagent-response-time');
+
+const { expect } = chai;
+
+describe('When we try to get all the users from github', () => {
+  const usersUrl = 'https://api.github.com/users';
+  let getResponseTime;
+
+  before(() => agent
+    .get(usersUrl)
+    .use(responseTime((err, time) => {
+      getResponseTime = time;
+    })));
+
+  it('The response time for the request muts be lower than 5 seconds', () => {
+    expect(getResponseTime).to.be.below(5000);
+  });
+});
